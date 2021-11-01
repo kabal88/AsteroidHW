7fa86ced582896f7bcafc2b7e46a860b2a219767
Alexander K <askabalkin@gmail.com>
2021-11-01 11:42:50 +0300

    add unity project

---
+++ Assets/Scripts/Model/InputModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+namespace Asteroids.Model
+{
+    public class InputModel : MonoBehaviour
+    {
+        private Camera _camera;
+
+        private void Awake()
+        {
+            _camera = Camera.main;
+        }
+
+        public Vector3 GetAimTarget()
+        {
+            return Input.mousePosition - _camera.WorldToScreenPoint(transform.position);
+        }
+        
+        
+    }
+}
